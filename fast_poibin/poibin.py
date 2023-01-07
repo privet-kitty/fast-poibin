18a5f800057ecf6fabda1a3891f93a3bc6cd936a
Hugo Sansaqua <privet.kitty99@gmail.com>
2023-01-07 21:37:01 +0900

    Introduce DP_THRESHOLD

--- fast_poibin/poibin.py
+++ fast_poibin/poibin.py
@@ -70,12 +70,18 @@ def calc_pmf_dp(probabilities: npt.NDArray[np.float64]) -> npt.NDArray[np.float6
     return dp
 
 
-# Use numpy.convolve instead of FFT under this threshold. This value was decided
+# calc_pmf uses numpy.convolve instead of FFT under this threshold. This value was decided
 # based on the experiment in https://github.com/privet-kitty/fast-poibin/issues/1.
 FFT_THRESHOLD = 1024
 
+# calc_pmf does DP instead of divide-and-conquer method under this threshold. This value
+# was decided based on the experiment in https://github.com/privet-kitty/fast-poibin/issues/2
+DP_THRESHOLD = 128
 
-def calc_pmf(probabilities: RealSequence, dp_threshold: int = 0) -> npt.NDArray[np.float64]:
+
+def calc_pmf(
+    probabilities: RealSequence, dp_threshold: int = DP_THRESHOLD
+) -> npt.NDArray[np.float64]:
     """Calculate PMF of Poisson binomial distribution.
 
     Time complexity: O(N(logN)^2)
