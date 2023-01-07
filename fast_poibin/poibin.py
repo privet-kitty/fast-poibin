5bd1288bec98143724c145bbe6fa415b3ce237e0
Hugo Sansaqua <privet.kitty99@gmail.com>
2023-01-07 18:08:45 +0900

    [skip ci] Fix URL in comment

--- fast_poibin/poibin.py
+++ fast_poibin/poibin.py
@@ -36,7 +36,7 @@ def convolve_power_of_two_degree(
     # Suppose we want to get f(x) * g(x). Let f(x) = a + x * f'(x) and g(x) = b + x * g'(x).
     # Then f(x) * g(x) = a * b + x * (b * f'(x) + a * g'(x)) + x^2 * f'(x) * g'(x).
     # I learned this technique from
-    # https://maspypy.com/%E5%A4%9A%E9%A0%85%E5%BC%8F%E3%83%BB%E5%BD%A2%E5%BC%8F%E7%9A%84%E3%81%B9%E3%81%8D%E7%B4%9A%E6%95%B0-%E9%AB%98%E9%80%9F%E3%81%AB%E8%A8%88%E7%AE%97%E3%81%A7%E3%81%8D%E3%82%8B%E3%82%82%E3%81%AE
+    # https://maspypy.com/%E5%A4%9A%E9%A0%85%E5%BC%8F%E3%83%BB%E5%BD%A2%E5%BC%8F%E7%9A%84%E3%81%B9%E3%81%8D%E7%B4%9A%E6%95%B0-%E9%AB%98%E9%80%9F%E3%81%AB%E8%A8%88%E7%AE%97%E3%81%A7%E3%81%8D%E3%82%8B%E3%82%82%E3%81%AE#toc17
     a = vector1[0]
     b = vector2[0]
     res[0] = a * b
