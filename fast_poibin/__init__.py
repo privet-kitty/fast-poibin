8ea8c3620819b3dadc4b333d4cd5024511ae14d7
Hugo Sansaqua <privet.kitty99@gmail.com>
2023-01-09 08:14:35 +0900

    Explicitly reexport modules from __init__ to convince mypy

--- fast_poibin/__init__.py
+++ fast_poibin/__init__.py
@@ -1,1 +1,5 @@
-from fast_poibin.poibin import PoiBin
+# FIXME: I use "as" here to comply with mypy's no-implicit-reexport rule, which
+# surprisingly disallows the implicit reexport even in __init__ module. Since
+# mypy --strict contains this rule, it's not negligible.
+# Please see https://mypy.readthedocs.io/en/stable/command_line.html#cmdoption-mypy-no-implicit-reexport.
+from fast_poibin.poibin import PoiBin as PoiBin
