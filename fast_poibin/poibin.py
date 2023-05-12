eef9eb837c180853445d18b500afd26ccb47fcbd
Hugo Sansaqua <privet.kitty99@gmail.com>
2023-05-13 07:28:01 +0900

    Add dp mode

--- fast_poibin/poibin.py
+++ fast_poibin/poibin.py
@@ -1,7 +1,9 @@
+from typing import Literal, Union
+
 import numpy as np
 import numpy.typing as npt
 
-from fast_poibin.pmf import FloatSequence, calc_pmf
+from fast_poibin.pmf import FloatSequence, calc_pmf, calc_pmf_dp
 
 
 def _validate_probabilities(probabilities: FloatSequence) -> None:
@@ -34,8 +36,8 @@ class PoiBin:
         also https://numpy.org/doc/1.24/reference/routines.fft.html#type-promotion.
 
     Notes:
-        The internal algorithm is based on the well-known divide-and-conquer approach
-        for convolving many polynomials. Its complexities are as follows.
+        The internal algorithm  for `mixed` mode is based on the well-known divide-and-conquer
+        approach for convolving many polynomials. Its complexities are as follows.
 
         - Time: O(N(logN)^2)
         - Space: O(N)
@@ -46,15 +48,31 @@ class PoiBin:
         Biscarri, William & Zhao, Sihai & Brunner, Robert. (2018). A simple and fast
         method for computing the Poisson binomial distribution function. Computational
         Statistics & Data Analysis. 122. 10.1016/j.csda.2018.01.007.
+
+        A downside of this approach is numerical precision. Since convolution by FFT
+        is unreliable in the world under the float epsilon (i.e. 2^-52 ~ 2 * 10^-16),
+        the relative errors of the computed PMF could be large though it is still not
+        bad w.r.t. the absolute errors. For details, please see the following issue:
+        https://github.com/privet-kitty/fast-poibin/issues/9. For situations where the
+        relative precision is required, you may want to use `dp` mode instead. You
+        should also note that neither mode can express a probability mass below the
+        least positive double float (~ 5 * 10^-324).
     """
 
-    def __init__(self, probabilities: FloatSequence) -> None:
+    def __init__(
+        self, probabilities: FloatSequence, mode: Union[Literal["mixed"], Literal["dp"]] = "mixed"
+    ) -> None:
         """
         Args:
             probabilities: Success probabilities for each binomial trial.
         """
         _validate_probabilities(probabilities)
-        self.pmf = calc_pmf(probabilities)
+        if mode == "mixed":
+            self.pmf = calc_pmf(probabilities)
+        elif mode == "dp":
+            self.pmf = calc_pmf_dp(np.array(probabilities, np.float64))
+        else:
+            raise ValueError(f"Invalid mode: {mode}")
         self.cdf: npt.NDArray[np.float64] = np.minimum(np.cumsum(self.pmf), 1.0)
         self.cdf[-1] = 1.0
 
