91b43be269a4acb66a0feca0e0a2adc2bd2b4dd5
Hugo Sansaqua <privet.kitty99@gmail.com>
2023-05-13 07:33:39 +0900

    Add tests for dp mode

--- tests/test_poibin.py
+++ tests/test_poibin.py
@@ -7,52 +7,57 @@ from fast_poibin.poibin import PoiBin
 
 
 def test_poibin_empty() -> None:
-    poibin = PoiBin([])
-    nptest.assert_array_almost_equal(poibin.pmf, [1])
-    nptest.assert_array_almost_equal(poibin.cdf, [1])
-    assert poibin.quantile(0) == 0
-    assert poibin.quantile(0.05) == 0
-    assert poibin.quantile(0.1) == 0
-    assert poibin.quantile(0.9) == 0
-    assert poibin.quantile(1) == 0
-    nptest.assert_array_equal(poibin.quantile([0.1, 0.2, 0.05, 1, 0]), [0] * 5)
+    for mode in ("mixed", "dp"):
+        poibin = PoiBin([], mode=mode)  # type: ignore
+        nptest.assert_array_almost_equal(poibin.pmf, [1])
+        nptest.assert_array_almost_equal(poibin.cdf, [1])
+        assert poibin.quantile(0) == 0
+        assert poibin.quantile(0.05) == 0
+        assert poibin.quantile(0.1) == 0
+        assert poibin.quantile(0.9) == 0
+        assert poibin.quantile(1) == 0
+        nptest.assert_array_equal(poibin.quantile([0.1, 0.2, 0.05, 1, 0]), [0] * 5)
 
 
 def test_poibin_single() -> None:
-    poibin = PoiBin([0.9])
-    nptest.assert_array_almost_equal(poibin.pmf, [0.1, 0.9])
-    nptest.assert_array_almost_equal(poibin.cdf, [0.1, 1])
-    assert poibin.quantile(0) == 0
-    assert poibin.quantile(0.05) == 0
-    assert poibin.quantile(0.1) == 1
-    assert poibin.quantile(0.9) == 1
-    assert poibin.quantile(1) == 1
-    nptest.assert_array_equal(poibin.quantile([0.1, 0.2, 0.05, 1, 0]), [1, 1, 0, 1, 0])
+    for mode in ("mixed", "dp"):
+        poibin = PoiBin([0.9], mode=mode)  # type: ignore
+        nptest.assert_array_almost_equal(poibin.pmf, [0.1, 0.9])
+        nptest.assert_array_almost_equal(poibin.cdf, [0.1, 1])
+        assert poibin.quantile(0) == 0
+        assert poibin.quantile(0.05) == 0
+        assert poibin.quantile(0.1) == 1
+        assert poibin.quantile(0.9) == 1
+        assert poibin.quantile(1) == 1
+        nptest.assert_array_equal(poibin.quantile([0.1, 0.2, 0.05, 1, 0]), [1, 1, 0, 1, 0])
 
 
 def test_poibin_all_zero() -> None:
-    poibin = PoiBin([0, 0, 0, 0, 0])
-    nptest.assert_array_equal(poibin.pmf, [1, 0, 0, 0, 0, 0])
-    nptest.assert_array_equal(poibin.cdf, [1] * 6)
-    assert poibin.quantile(0) == 0
-    assert poibin.quantile(0.5) == 0
-    assert poibin.quantile(1) == 0
+    for mode in ("mixed", "dp"):
+        poibin = PoiBin([0, 0, 0, 0, 0], mode=mode)  # type: ignore
+        nptest.assert_array_equal(poibin.pmf, [1, 0, 0, 0, 0, 0])
+        nptest.assert_array_equal(poibin.cdf, [1] * 6)
+        assert poibin.quantile(0) == 0
+        assert poibin.quantile(0.5) == 0
+        assert poibin.quantile(1) == 0
 
 
 def test_poibin_all_one() -> None:
-    poibin = PoiBin([1, 1, 1])
-    nptest.assert_array_equal(poibin.pmf, [0, 0, 0, 1])
-    nptest.assert_array_equal(poibin.cdf, [0, 0, 0, 1])
-    assert poibin.quantile(0) == 0
-    assert poibin.quantile(0.5) == 3
-    assert poibin.quantile(1) == 3
+    for mode in ("mixed", "dp"):
+        poibin = PoiBin([1, 1, 1], mode=mode)  # type: ignore
+        nptest.assert_array_equal(poibin.pmf, [0, 0, 0, 1])
+        nptest.assert_array_equal(poibin.cdf, [0, 0, 0, 1])
+        assert poibin.quantile(0) == 0
+        assert poibin.quantile(0.5) == 3
+        assert poibin.quantile(1) == 3
 
 
 def test_poibin_non_float64() -> None:
     """PoiBin stores float64 even when the argument is not."""
-    poibin = PoiBin(np.array([0.2, 0.5], np.float32))
-    assert poibin.pmf.dtype == np.float64
-    assert poibin.cdf.dtype == np.float64
+    for mode in ("mixed", "dp"):
+        poibin = PoiBin(np.array([0.2, 0.5], np.float32), mode=mode)  # type: ignore
+        assert poibin.pmf.dtype == np.float64
+        assert poibin.cdf.dtype == np.float64
 
 
 def test_poibin_non_1d_array_is_forbidden() -> None:
@@ -71,3 +76,10 @@ def test_poibin_non_probablities() -> None:
     check(np.array([-0.5]))
     check([1.5])
     check([-0.5])
+
+
+def test_poibin_dp_mode_mixed_mode_equivalence() -> None:
+    poibin_dp = PoiBin([0.1, 0.2, 0.3, 0.4], mode="dp")
+    poibin_mixed = PoiBin([0.1, 0.2, 0.3, 0.4], mode="mixed")
+    nptest.assert_array_almost_equal(poibin_dp.pmf, poibin_mixed.pmf)
+    nptest.assert_array_almost_equal(poibin_dp.cdf, poibin_mixed.cdf)
